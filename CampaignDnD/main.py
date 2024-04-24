ca120f2ef96e790079ce366df73ee743d4409334
Orgzales <77345323+Orgzales@users.noreply.github.com>
2024-04-24 18:31:27 -0400

    Combining Code Loops

--- CampaignDnD/main.py
+++ CampaignDnD/main.py
@@ -51,10 +51,10 @@ with progress:
         spell_counts_per_file[file_path] = spell_counts
         progress.advance(task)
 
-for file_path, counts in spell_counts_per_file.items():
-    print("File: " + str(file_path))
-    for spell, count in counts.items():
-        print(spell + ": " + str(count))
+# for file_path, counts in spell_counts_per_file.items():
+#     print("File: " + str(file_path))
+#     for spell, count in counts.items():
+#         print(spell + ": " + str(count))
 
 # Count the total occurrences of each spell and print the total with the spell name
 Spell_total_counts = {}
@@ -72,43 +72,63 @@ print("Files with 'level up':", files_with_level_up)
 
 print("!!!!!!!!!!!!!!!!!!!!!!")
 # print the total count of each spell only if is greater than 0
-for spell, count in Spell_total_counts.items():
-    if count > 0:
-        print(spell + ": " + str(count))
-        #print out the calsses value for each spell
-        print(spell_classes_values[spell_names_values.tolist().index(spell)])
+# for spell, count in Spell_total_counts.items():
+#     if count > 0:
+#         print(spell + ": " + str(count))
+#         #print out the calsses value for each spell
+#         print(spell_classes_values[spell_names_values.tolist().index(spell)])
 
 df_characters = pd.read_csv("Charaters.csv")
 
-character_names = df_characters['Name'].unique()  # Unique character names
-character_classes = df_characters['Class'].unique()  # Unique character classes
-character_spells = df_characters['Spells'].unique()  # Unique character spells
+character_names = df_characters['Name']  # Unique character names
+character_classes = df_characters['Class']  # Unique character classes
+character_spells = df_characters['Spells']  # Unique character spells
 
 character_success_rate = {} #store the success rate of each character
 
 #Check if each charater has the spell or not | or class can have the spell
 for character in character_names:
     sucesss_rate = 0.0 #testing
-    character_class = character_classes[character_names.tolist().index(character)]
-    class_text = character_class.split(" ")[0] #to get rid of the extra text in the class column
+    # character_class = character_classes[character_names.tolist().index(character)]
+    # class_text = character_class.split(" ")[0] #to get rid of the extra text in the class column
     # level_text = character_class.split(" ")[1] #to get the level to add to the susscess rate
     # sucesss_rate = sucesss_rate + int(level_text) #add the level to the success rate
 
-    for spell in spell_counts: #go throgh each spell (change to each session later)
-        if Spell_total_counts[spell] > 0: # if it has been used in the session
-            spell_occurance = Spell_total_counts[spell] # total num of times the spell has been used
-            spell_class = spell_classes_values[spell_names_values.tolist().index(spell)] # the classes that could use the spell
-            # print ("Spell: " + spell)
-            if spell in character_spells[character_names.tolist().index(character)]:
-                print(character + " has " + spell + " spell")
-                sucesss_rate = (spell_occurance * 2) + sucesss_rate
-            elif class_text in spell_class:
-                print(character_classes[character_names.tolist().index(character)])
-                print(spell_class)
-                print(character + " can obtain " + spell + " spell")
-                sucesss_rate = spell_occurance + sucesss_rate
-                # print("Spell class: " + spell_class)
-                # print("Character class: " + character_classes[character_names.tolist().index(character)])
+    character_class = character_classes[character_names.tolist().index(character)]
+    class_text = character_class.split("|")  # to get rid of the extra text in the class column
+    print(class_text)
+    # level_text = character_class.split(" ")[1] #to get the level to add to the susscess rate
+    # sucesss_rate = sucesss_rate + int(level_text) #add the level to the success rate
+    for class_name in class_text:
+        name_class = class_name.split(" ")[0]
+        level_class = class_name.split(" ")[1]
+        # print(name_class)
+        # print(level_class)
+        for spell in spell_counts: #go throgh each spell (change to each session later)
+            if Spell_total_counts[spell] > 0: # if it has been used in the session
+                spell_occurance = Spell_total_counts[spell] # total num of times the spell has been used
+                spell_class = spell_classes_values[spell_names_values.tolist().index(spell)] # the classes that could use the spell
+                # print ("Spell: " + spell)
+                if spell in character_spells[character_names.tolist().index(character)]:
+                    print(character + " has " + spell + " spell")
+                    sucesss_rate = (spell_occurance * 2) + sucesss_rate
+                elif class_name in spell_class:
+                    print(character_classes[character_names.tolist().index(character)])
+                    print(spell_class)
+                    print(character + " can obtain " + spell + " spell")
+                    sucesss_rate = spell_occurance + sucesss_rate
+                    # print("Spell class: " + spell_class)
+                    # print("Character class: " + character_classes[character_names.tolist().index(character)])
+    character_skills = df_characters['Skills'][character_names.tolist().index(character)]
+    print(character + " skills: " + character_skills)
+    skills_split = character_skills.split("|")
+    for skill in skills_split:
+        print(skill)
+        for word, count in word_counts.items():
+            if skill.lower() in word:
+                print(word + ": " + str(count))
+
+
     print("success rate for " + character + " is: " + str(sucesss_rate) + "%")
     character_success_rate[character] = sucesss_rate
 
@@ -119,20 +139,20 @@ for character, rate in sorted(character_success_rate.items(), key=lambda item: i
     print(character + ": " + str(rate) + "%")
 
 
-print("!!!!!!!!!!!!!!!!!!!!!!")
-#getting the level and class of each subclass
-for character in character_names:
-    character_class = character_classes[character_names.tolist().index(character)]
-    class_text = character_class.split("|")  # to get rid of the extra text in the class column
-    print (class_text)
-    # level_text = character_class.split(" ")[1] #to get the level to add to the susscess rate
-    # sucesss_rate = sucesss_rate + int(level_text) #add the level to the success rate
-    for string in class_text:
-        name_class = string.split(" ")[0]
-        level_class = string.split(" ")[1]
-        print(name_class)
-        print(level_class)
-    print("!!!!!!!!!!!!!!!!!!!!!!")
+# print("!!!!!!!!!!!!!!!!!!!!!!")
+# #getting the level and class of each subclass
+# for character in character_names:
+#     character_class = character_classes[character_names.tolist().index(character)]
+#     class_text = character_class.split("|")  # to get rid of the extra text in the class column
+#     print (class_text)
+#     # level_text = character_class.split(" ")[1] #to get the level to add to the susscess rate
+#     # sucesss_rate = sucesss_rate + int(level_text) #add the level to the success rate
+#     for string in class_text:
+#         name_class = string.split(" ")[0]
+#         level_class = string.split(" ")[1]
+#         print(name_class)
+#         print(level_class)
+#     print("!!!!!!!!!!!!!!!!!!!!!!")
 
 
 # print out the top 10 most common words
@@ -142,16 +162,16 @@ for word, count in sorted(word_counts.items(), key=lambda item: item[1], reverse
     print(word + ": " + str(count))
 
 # for each charater, find how often their skills would be used in the sesssions
-print("!!!!!!!!!!!!!!!!!!!!!!")
-for character in character_names:
-    character_skills = df_characters['Skills'][character_names.tolist().index(character)]
-    print(character + " skills: " + character_skills)
-    skills_split = character_skills.split("|")
-    for skill in skills_split:
-        print(skill)
-        for word, count in word_counts.items():
-            if skill.lower() in word:
-                print(word + ": " + str(count))
+# print("!!!!!!!!!!!!!!!!!!!!!!")
+# for character in character_names:
+#     character_skills = df_characters['Skills'][character_names.tolist().index(character)]
+#     print(character + " skills: " + character_skills)
+#     skills_split = character_skills.split("|")
+#     for skill in skills_split:
+#         print(skill)
+#         for word, count in word_counts.items():
+#             if skill.lower() in word:
+#                 print(word + ": " + str(count))
 
 
 df_monsters = pd.read_csv("DndData/Dd5e_monsters.csv")
@@ -195,6 +215,7 @@ for file_path, counts in monster_count.items():
     for monster, count in counts.items():
         if count > 0:
             print(monster + ": " + str(count))
+            print("Monster CR: " + str(df_monsters['Challenge rating  (XP)'][monster_names.tolist().index(monster)]))
 
 
 
